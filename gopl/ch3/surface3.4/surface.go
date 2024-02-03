9d3a5504eb80127230bbc59151ab2fed1057fd25
Igor Mozhaev <grifonhard@gmail.com>
2024-02-03 10:11:23 +0300

    img

--- gopl/ch3/surface3.4/surface.go
+++ gopl/ch3/surface3.4/surface.go
@@ -6,22 +6,20 @@ import (
 	"log"
 	"math"
 	"net/http"
+	"strconv"
 )
 
 const (
-	width, height = 600, 320            // canvas size in pixels
-	cells         = 100                 // number of grid cells
-	xyrange       = 30.0                // x, y axis range (-xyrange..+xyrange)
-	xyscale       = width / 2 / xyrange // pixels per x or y unit
-	zscale        = height * 0.4        // pixels per z unit
-	angle         = math.Pi / 6
+	cells   = 100  // number of grid cells
+	xyrange = 30.0 // x, y axis range (-xyrange..+xyrange)
+	angle   = math.Pi / 6
 )
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle)
 
 //func for coordinate calculation
 
-func corner(i, j int) (float64, float64) {
+func corner(i, j, width, height int) (float64, float64) {
 	// find point (x,y) at corner of cell (i,j)
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
@@ -29,8 +27,10 @@ func corner(i, j int) (float64, float64) {
 	z := f(x, y) // compute surface height z
 
 	// project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy)
-	sx := width/2 + (x-y)*cos30*xyscale
-	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	xyscale := float64(width) / 2 / xyrange // pixels per x or y unit
+	zscale := float64(height) * 0.4         // pixels per z unit
+	sx := float64(width)/2 + (x-y)*cos30*xyscale
+	sy := float64(height)/2 + (x+y)*sin30*xyscale - z*zscale
 	return sx, sy
 }
 
@@ -41,16 +41,16 @@ func f(x, y float64) float64 {
 
 //func for post in response
 
-func surface(w io.Writer, color string) {
+func surface(w io.Writer, color string, width int, height int) {
 	firstString := "<svg xmlns='http://www.w3.org/2000/svg' " +
 		"style='stroke: grey; fill: " + color + "; stroke-width: 0.7' "
 	fmt.Fprintf(w, firstString+"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
+			ax, ay := corner(i+1, j, width, height)
+			bx, by := corner(i, j, width, height)
+			cx, cy := corner(i, j+1, width, height)
+			dx, dy := corner(i+1, j+1, width, height)
 			fmt.Fprintf(w, "<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
 				ax, ay, bx, by, cx, cy, dx, dy)
 		}
@@ -60,12 +60,18 @@ func surface(w io.Writer, color string) {
 
 func draw(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	color := r.Header.Get("color")
-	w.Header().Set("ContentType", "image/svg+xml")
-	surface(w, color)
+	color := r.FormValue("color")
+	widthS := r.FormValue("width")
+	width, _ := strconv.Atoi(widthS)
+	heightS := r.FormValue("height")
+	height, _ := strconv.Atoi(heightS)
+	w.Header().Set("Content-Type", "image/svg+xml")
+	surface(w, color, width, height)
 }
 
 func main() {
 	http.HandleFunc("/draw", draw)
 	log.Fatal(http.ListenAndServe("localhost:8010", nil))
 }
+
+//http://localhost:8010/draw?color=white&width=600&height=600
